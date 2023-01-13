1e52d2840cd932d6f22ef50e7ac0ee317262a0df
mort1skoda <mort1skoda@gmail.com>
2023-01-13 18:16:00 +0100

    2023-01-13 18:16:00 Friday

--- gp.sh
+++ gp.sh
@@ -6,7 +6,7 @@ git status
 DATE=$(date +'%Y-%m-%d %H:%M:%S %aday')
 git commit -m "$DATE"
 git branch -M master
-#git remote add origin https://github.com/mort1skoda/devC.git
+#git remote add origin https://github.com/mort1skoda/devHtml.git
 git push -u origin master
 
 #TODO:
